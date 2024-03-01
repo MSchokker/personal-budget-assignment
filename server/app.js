74f625db2c18c6a3e28fa1164a865c150b96862d
MSchokker <matthijs.schokker@linkit.nl>
2024-03-01 16:52:36 +0100

    added end point and logic to create new envelopes

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
     res.send('Hello, World');
 })
 
 const envelopeRouter = require('./envelopes.js');
-app.use('/envelopes', envelopeRouter)
+app.use('/envelopes', envelopeRouter);
 
 console.log(`Server listening at Port: ${PORT}`);
 app.listen(PORT);
--- server/envelopes.js
+++ server/envelopes.js
@@ -1,11 +1,34 @@
+const { create } = require('domain');
 const express = require('express');
 const envelopeRouter = express.Router();
 
 const envelopes = [];
 let envelopeCounter = 1;
 
+const createEnvelope = (title, budget) => {
+    if(title && budget) {
+        return {
+            id: `${envelopeCounter++}`,
+            title: title,
+            budget: budget
+        } 
+    } else {
+        return null;
+    }
+};
+
 module.exports = envelopeRouter;
 
 envelopeRouter.get('/', (req, res, next) => {
     res.send(envelopes);
+});
+
+envelopeRouter.post('/', (req, res, next) => {
+    const envelope = createEnvelope(req.body.title, Number(req.body.budget));
+    if(envelope) {
+        envelopes.push(envelope);
+        res.status(201).send(envelope);
+    } else {
+        res.status(400).send();
+    }
 });
