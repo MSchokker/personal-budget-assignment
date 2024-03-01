2b5f0d627522eebdbb6ab71aca22621d9c95dae3
MSchokker <matthijs.schokker@linkit.nl>
2024-03-01 16:33:33 +0100

    added a get method for retrieving all envelopes

--- server/envelopes.js
+++ server/envelopes.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const envelopeRouter = express.Router();
 
+const envelopes = [];
+let envelopeCounter = 1;
+
 module.exports = envelopeRouter;
+
+envelopeRouter.get('/', (req, res, next) => {
+    res.send(envelopes);
+});
